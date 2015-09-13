d5f2f26ae68738328c2101789a7d26dd4b8db4b5
yanninho <y4nn5m@gmail.com>
2015-09-13 23:24:41 +0200

    serve static

--- web.js
+++ web.js
@@ -3,12 +3,14 @@
 var express = require('express');
 var app = express();
 var compression = require('compression');
-var modRewrite = require('connect-modrewrite');
+var serveStatic = require('serve-static');
+// var modRewrite = require('connect-modrewrite');
 
 app.use(compression());
-app.use(modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.woff|\\.ttf$ ' + __dirname + "/dist/index.html" + ' [L]']));
+app.use(serveStatic("" + __dirname + '/dist'));
+// app.use(modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.woff|\\.ttf$ ' + __dirname + "/dist/index.html" + ' [L]']));
 app.listen(process.env.PORT || 5000);
 
-// app.get('*', function(req,res,next) {
-// 	res.sendfile(__dirname + 'dist/index.html');
+// app.all('/*', function(req,res,next) {
+// 	res.sendfile(__dirname + '/dist/index.html');
 // });
