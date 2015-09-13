514e0160b30166d362ef82b2a5a4bd983a6f59a8
yanninho <y4nn5m@gmail.com>
2015-09-13 23:27:32 +0200

    rewrite again

--- web.js
+++ web.js
@@ -4,11 +4,11 @@ var express = require('express');
 var app = express();
 var compression = require('compression');
 var serveStatic = require('serve-static');
-// var modRewrite = require('connect-modrewrite');
+var modRewrite = require('connect-modrewrite');
 
 app.use(compression());
 app.use(serveStatic("" + __dirname + '/dist'));
-// app.use(modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.woff|\\.ttf$ ' + __dirname + "/dist/index.html" + ' [L]']));
+app.use(modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.woff|\\.ttf$ ' + __dirname + "/dist/index.html" + ' [L]']));
 app.listen(process.env.PORT || 5000);
 
 // app.all('/*', function(req,res,next) {
