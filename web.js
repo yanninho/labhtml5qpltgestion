89db0245f769d7560188e3eeb64a2aba02691f8e
yanninho <y4nn5m@gmail.com>
2015-09-13 22:25:40 +0200

    conf modrewrite

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express();
 var modRewrite = require('connect-modrewrite');
 
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.use(modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.woff|\\.ttf$ /index.html [L]']));
+app.use(modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.woff|\\.ttf$ /dist/index.html [L]']));
 app.listen(process.env.PORT || 5000);
 
 // app.get('*', function(req,res,next) {
