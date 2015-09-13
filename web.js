6bd6f3b081fcf95a913b65dd00e7608fea22c938
yanninho <y4nn5m@gmail.com>
2015-09-13 23:05:59 +0200

    modrewrite test

--- web.js
+++ web.js
@@ -3,10 +3,10 @@
 var express = require('express');
 var app = express();
 var compression = require('compression');
-// var modRewrite = require('connect-modrewrite');
+var modRewrite = require('connect-modrewrite');
 
 app.use(compression());
-// app.use(modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.woff|\\.ttf$ ' + __dirname + "/dist" + ' [L]']));
+app.use(modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.woff|\\.ttf$ ' + __dirname + "/dist/index.html" + ' [L]']));
 app.listen(process.env.PORT || 5000);
 
 // app.get('*', function(req,res,next) {
