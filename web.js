89f45002eb8530c151e6617515937de845befefe
yanninho <y4nn5m@gmail.com>
2015-09-13 22:47:09 +0200

    rewrite url

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 var modRewrite = require('connect-modrewrite');
 
-app.use(modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.woff|\\.ttf$ /index.html [L]']));
+app.use(modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.woff|\\.ttf$ ' + __dirname + "/dist" + ' [L]']));
 app.listen(process.env.PORT || 5000);
 
 // app.get('*', function(req,res,next) {
